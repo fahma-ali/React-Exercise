75865516844b6f00e7aa64f943a46652a174b2aa
halbeegaccandconsultancy-design <halbeeg.accandconsultancy@gmail.com>
2026-06-20 08:24:44 +0300

    exercise3

---
+++ exercise3/src/App.jsx
@@ -0,0 +1,16 @@
+import UserCard from "./UserCard"
+
+const App =()=>{
+  return(
+    <>
+    <UserCard userName={"Fahma"}
+    UserEmail={"fahma@gmail.com"}/>
+     <UserCard userName={"nasra"}
+    UserEmail={"nasra@gmail.com"}/>
+     <UserCard userName={"ahmed"}
+    UserEmail={"ahmed@gmail.com"}/>
+    </>
+  )
+
+}
+export default App
---
+++ exercise3/src/UserCard.jsx
@@ -0,0 +1,10 @@
+const UserCard = ({userName,UserEmail}) => {
+    return (
+        <>
+        <h1>User Name is : {userName}</h1>
+        <p>User Email is :{UserEmail}</p>
+
+    </>
+    )
+}
+export default UserCard
