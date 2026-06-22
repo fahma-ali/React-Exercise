bba6335a6cc4ea6951ffeb36a6030c8d084f1e1f
halbeegaccandconsultancy-design <halbeeg.accandconsultancy@gmail.com>
2026-06-22 12:39:12 +0300

    exercise5

--- exercise5/src/ShoppingCart.jsx
+++ exercise5/src/ShoppingCart.jsx
@@ -3,18 +3,17 @@ const ShoppingCart = () => {
   const [listCart, setListCart] = useState([]);
   const [cartInput, setCartInput] = useState({ name: "", price: "" });
   const handleAddCart = () => {
-    if(cartInput.name.trim() === "" || isNaN(Number(cartInput.price))) {
-        return;
+    if (cartInput.name.trim() !== "" && cartInput.price.trim() !== "") {
+      const newShoppingCart = {
+        id: crypto.randomUUID(),
+        name: cartInput.name,
+        price: cartInput.price,
+        qty: 1,
+      };
+        setListCart([...listCart, newShoppingCart]);
+    setCartInput({ name: "", price: "" });
     }
-
-    const newShoppingCart = {
-      id: crypto.randomUUID(),
-      name: cartInput.name,
-      price: cartInput.price,
-      qty: 1,
-    };
-    setListCart([...listCart, newShoppingCart]);
-    setCartInput({name:"", price: ""});
+  
   };
   const handleChange = (event) => {
     setCartInput({ ...cartInput, [event.target.name]: event.target.value });
@@ -26,69 +25,69 @@ const ShoppingCart = () => {
       ),
     );
   };
-  const handleDecreaseQty= (id) => {
+  const handleDecreaseQty = (id) => {
     setListCart(
-        listCart.map((item) => item.id === id && item.qty > 1 ? { ...item, qty:item.qty-1}:item)
-    )
-  }
-  const handleRemove=(id) => {
-    setListCart(
-        listCart.filter(item => item.id !== id),
-    )
-  }
-  const TotalPrice=listCart.reduce((total, item)=>total+Number(item.price)*item.qty, 0);
+      listCart.map((item) =>
+        item.id === id && item.qty > 1 ? { ...item, qty: item.qty - 1 } : item,
+      ),
+    );
+  };
+  const handleRemove = (id) => {
+    setListCart(listCart.filter((item) => item.id !== id));
+  };
+  const TotalPrice = listCart.reduce(
+    (total, item) => total + Number(item.price) * item.qty,
+    0,
+  );
   return (
     <div>
       <h1>Simple Shopping Cart</h1>
-      <h2>Add Product</h2>
-      <input
-        type="text"
-        name="name"
-        id="name"
-        onChange={handleChange}
-        value={cartInput.name}
-        placeholder="Enter Product"
-      />
-      <input
-        type="number"
-        name="price"
-        id="price"
-        onChange={handleChange}
-        value={cartInput.price}
-        placeholder="Enter Product Price"
-      />
-      <button onClick={handleAddCart}>Add To Cart</button>
-      {
-        listCart.length === 0 ? (
-            <h6>this cart is empty</h6>
-        ) :(
-            <>
-   <h4>Product In Cart</h4>
-      <ul>
-        {
-            
-        listCart.map((item) => (
-            
-          <li key={item.id}>
-                    
-
-            {item.name}-${item.price}
-            <p>
-              Quantity: <button onClick={()=> handleDecreaseQty(item.id)}>-</button>
-              {item.qty}
-              <button onClick={() => handleAddQty(item.id)}>+</button>
-              <button onClick={()=>handleRemove(item.id)}>Remove</button>
-            </p>
-           
-          </li>
-        ))}
-      </ul>
-                 <h1>Total Price :{TotalPrice}</h1>
-            </>
-        )
-      }
-   
-
+      <div>
+        <h2>Add Product</h2>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={handleChange}
+          value={cartInput.name}
+          placeholder="Enter Product"
+        />
+        <input
+          type="number"
+          name="price"
+          id="price"
+          onChange={handleChange}
+          value={cartInput.price}
+          placeholder="Enter Product Price"
+        />
+        <button onClick={handleAddCart}>Add To Cart</button>
+        {listCart.length === 0 ? (
+          <h6>this cart is empty</h6>
+        ) : (
+          <>
+            <h4>Product In Cart</h4>
+            <ul>
+              {listCart.map((item) => (
+                <li key={item.id}>
+                  {item.name}-${item.price}
+                  <p>
+                    Quantity:{" "}
+                    <button onClick={() => handleDecreaseQty(item.id)}>
+                      -
+                    </button>
+                    {item.qty}
+                    <button onClick={() => handleAddQty(item.id)}>+</button>
+                    <button onClick={() => handleRemove(item.id)}>
+                      Remove
+                    </button>
+                  </p>
+                </li>
+              ))}
+            </ul>
+            <h1>Total Price :{TotalPrice}</h1>
+          </>
+        )}
+      </div>
     </div>
   );
 };
